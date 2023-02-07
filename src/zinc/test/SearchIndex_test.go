1872999a16ba9ac3a107a9becec6cf860ac0ca07
perqueza72 <practica22correo@gmail.com>
2023-02-07 16:50:23 -0500

    Update tests for searchindexer.

--- src/zinc/test/SearchIndex_test.go
+++ src/zinc/test/SearchIndex_test.go
@@ -1,6 +1,7 @@
 package test
 
 import (
+	mapping "mapping_zinc"
 	models "models_zinc"
 	"testing"
 	"zinc_handler"
@@ -14,11 +15,40 @@ func TestSearchRecords(t *testing.T) {
 		SortFields: []string{"-Date"},
 		IndexPage:  0,
 	}
-	body, err := zinc_handler.SearchRecords(&recordQuery)
+	body, err := zinc_handler.SearchAllRecordsBy(&recordQuery)
+
+	if err != nil {
+		t.Log(err)
+		t.Fail()
+	}
+	_, err = mapping.GetZincRecords(&body)
 	if err != nil {
 		t.Log(err)
 		t.Fail()
 	}
 
-	t.Log(string(body))
+	t.Logf("Search all records works.")
+}
+
+func TestSearchRecordsBadAuth(t *testing.T) {
+	t.Setenv("ZINC_ADMIN_USER", "unexisting_admin")
+	t.Setenv("ZINC_ADMIN_PASSWORD", "unexisting_password")
+
+	recordQuery := models.RecordQueryRequest{
+		SortFields: []string{"-Date"},
+		IndexPage:  0,
+	}
+	body, err := zinc_handler.SearchAllRecordsBy(&recordQuery)
+
+	if err != nil {
+		t.Log(err)
+		t.Fail()
+	}
+	_, err = mapping.GetZincRecords(&body)
+	if err != nil {
+		t.Logf("Authentication process works!!\n%v", err)
+		return
+	}
+
+	t.Fail()
 }
