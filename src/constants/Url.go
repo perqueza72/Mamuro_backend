497871d289d29dd5c1f6930b1d37239b9c4f6ef3
perqueza72 <practica22correo@gmail.com>
2023-02-07 16:48:27 -0500

    Add constants to project.

--- src/constants/Url.go
+++ src/constants/Url.go
@@ -2,6 +2,7 @@ package constants
 
 const ZINC_HOST_DEV = "http://localhost:4080"
 const ZINC_HOST = "http://localhost:4080"
+const ZINC_EMAIL_INDEX = "email"
 
 const HOST_DEV = "http://localhost:8080"
 const HOST_PROD = "http://localhost:8080"
