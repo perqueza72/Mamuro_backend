feae52b5d09870ad52d4f22787ba192152b0862b
perqueza72 <practica22correo@gmail.com>
2023-02-08 21:51:46 -0500

    Modify constants according to environment.

---
+++ src/constants/modlevel_dev.go
@@ -0,0 +1,10 @@
+// +build dev
+//go:build dev
+// +build dev
+
+package constants
+
+const ZINC_HOST = "http://localhost:4080"
+const ZINC_EMAIL_INDEX = "email_dev"
+
+const HOST_PORT = ":8080"
---
+++ src/constants/modlevel_prod.go
@@ -0,0 +1,9 @@
+//go:build !dev
+// +build !dev
+
+package constants
+
+const ZINC_HOST = "http://172.17.0.2:4080"
+const ZINC_EMAIL_INDEX = "email"
+
+const HOST_PORT = ":8080"
